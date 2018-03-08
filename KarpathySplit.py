5539f30bdf08b29663982ef6698f442eb3b2572c
wzn0828 <18001279185@163.com>
2018-03-08 10:51:30 +0800

    change annotations path

--- KarpathySplit.py
+++ KarpathySplit.py
@@ -5,31 +5,32 @@
 import json
 from random import shuffle, seed
 
-seed( 123 ) # Make it reproducible
+seed(123)  # Make it reproducible
 
 num_val = 5000
 num_test = 5000
 
-val = json.load( open('annotations/captions_val2014.json', 'r') )
-train = json.load( open('annotations/captions_train2014.json', 'r') )
+val = json.load(open('/media/samsumg_1tb/Image_Caption/Datasets/MSCOCO/annotations/captions_val2014.json', 'r'))
+train = json.load(open('/media/samsumg_1tb/Image_Caption/Datasets/MSCOCO/annotations/captions_train2014.json', 'r'))
 
 # Merge together
 imgs = val['images'] + train['images']
 annots = val['annotations'] + train['annotations']
 
-shuffle( imgs )
+shuffle(imgs)
 
 # Split into val, test, train
 dataset = {}
-dataset[ 'val' ] = imgs[ :num_val ]
-dataset[ 'test' ] = imgs[ num_val: num_val + num_test ]
-dataset[ 'train' ] = imgs[ num_val + num_test: ]
+dataset['val'] = imgs[:num_val]
+dataset['test'] = imgs[num_val: num_val + num_test]
+dataset['train'] = imgs[num_val + num_test:]
 
 # Group by image ids
 itoa = {}
 for a in annots:
     imgid = a['image_id']
-    if not imgid in itoa: itoa[imgid] = []
+    if imgid not in itoa:
+        itoa[imgid] = []
     itoa[imgid].append(a)
 
 
@@ -37,20 +38,20 @@ json_data = {}
 info = train['info']
 licenses = train['licenses']
 
-split = [ 'val', 'test', 'train' ]
+split = ['val', 'test', 'train']
 
 for subset in split:
-    
-    json_data[ subset ] = { 'type':'caption', 'info':info, 'licenses': licenses,
-                           'images':[], 'annotations':[] }
-    
-    for img in dataset[ subset ]:
-        
+
+    json_data[subset] = {'type': 'caption', 'info': info, 'licenses': licenses,
+                           'images': [], 'annotations': []}
+
+    for img in dataset[subset]:
+
         img_id = img['id']
-        anns = itoa[ img_id ]
-        
-        json_data[ subset ]['images'].append( img )
-        json_data[ subset ]['annotations'].extend( anns )
-        
-    json.dump( json_data[ subset ], open( 'annotations/karpathy_split_' + subset + '.json', 'w' ) )
+        anns = itoa[img_id]
+
+        json_data[subset]['images'].append(img)
+        json_data[subset]['annotations'].extend(anns)
+
+    json.dump(json_data[subset], open('data/annotations/karpathy_split_' + subset + '.json', 'w'))
 
