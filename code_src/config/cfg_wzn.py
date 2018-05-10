0a79b0637805ff513a4e4e9464c12602825bc112
wzn0828 <18001279185@163.com>
2018-05-10 18:04:15 +0800

    for some local configuration

--- code_src/config/cfg_wzn.py
+++ code_src/config/cfg_wzn.py
@@ -6,8 +6,8 @@ image_dir                   = '/media/samsumg_1tb/Image_Caption/Datasets/MSCOCO'
 captions_val_origin         = '/media/samsumg_1tb/Image_Caption/Datasets/MSCOCO/annotations/annotations_trainval2014/captions_val2014.json'
 captions_train_origin       = '/media/samsumg_1tb/Image_Caption/Datasets/MSCOCO/annotations/annotations_trainval2014/captions_train2014.json'
 splited_anno_path_prefix    = 'code_src/data/annotations/karpathy_split_'
-train_anno_path             = 'code_src/data/annotations/karpathy_split_train_small.json'  # path for train annotation json file
-val_anno_path               = 'code_src/data/annotations/karpathy_split_val_small.json'    # path for validation annotation json file
+train_anno_path             = 'code_src/data/annotations/karpathy_split_train.json'  # path for train annotation json file
+val_anno_path               = 'code_src/data/annotations/karpathy_split_val.json'    # path for validation annotation json file
 test_anno_path              = 'code_src/data/annotations/karpathy_split_test.json'   # path for test annotation json file
 
 #--------------------attention_model--------------------#
@@ -28,33 +28,39 @@ train_lr_decay_every        = 50                                        # decay 
 train_early_stop            = True
 train_figure_epoch          = 2
 
-#--------------------test--------------------#
-testOrnot                   = False
-test_pretrained_model       = 'Experiments/2018-3-10/models/adaptive-12.pkl'      # used pretrained model parameters in test
-
-#--------------------hyper parameters--------------------#
 # optimization
 # CNN fine-tuning
-opt_fine_tune_cnn_start_layer   = 0                                         # CNN fine-tuning layers from: [0-7]
+opt_fine_tune_cnn_start_layer   = 5                                         # CNN fine-tuning layers from: [0-7]
 opt_fine_tune_cnn_start_epoch   = 20                                        # start fine-tuning CNN after
 
-# Optimizer parameter
+# Optimizer parameter of rnn
 opt_rnn_optimization                = 'sgd'  #['adam','sgd']
-opt_rnn_adam_alpha                  = 0.8                                       # alpha in Adam
+opt_rnn_adam_alpha                  = 0.9                                       # alpha in Adam
 opt_rnn_adam_beta                   = 0.999                                     # beta in Adam
-opt_rnn_adam_learning_rate          = 5e-6                                    # learning rate for the whole model
+opt_rnn_adam_learning_rate          = 1e-6                                    # learning rate for the whole model
+opt_rnn_adam_weight_decay           = 0
 
-opt_rnn_sgd_learning_rate           = 5e-6
-opt_rnn_sgd_momentum                = 0.9
+opt_rnn_sgd_learning_rate           = 1e-6
+opt_rnn_sgd_momentum                = 0.99
+opt_rnn_sgd_weight_decay            = 0
 
 # Optimizer parameter of cnn
 opt_cnn_optimization            = 'sgd'  #['adam','sgd']
-opt_cnn_adam_alpha              = 0.8                                       # alpha in Adam
+opt_cnn_adam_alpha              = 0.9                                       # alpha in Adam
 opt_cnn_adam_beta               = 0.999
-opt_cnn_adam_learning_rate      = 1e-6
+opt_cnn_adam_learning_rate      = 5e-7
+opt_cnn_adam_weight_decay       = 0
 
-opt_cnn_sgd_learning_rate       = 1e-6
-opt_cnn_sgd_momentum            = 0.9
+opt_cnn_sgd_learning_rate       = 5e-7
+opt_cnn_sgd_momentum            = 0.99
+opt_cnn_sgd_weight_decay        = 0
+
+
+#--------------------test--------------------#
+testOrnot                   = False
+test_pretrained_model       = 'Experiments/Train_rnn_attention_lr_5e-06_cnnlr_1e-06_cnn_start_layer_0_cnn_start_epoch_20___2018-04-27-11-00-54/trained_models/adaptive-51.pkl'      # used pretrained model parameters in test
+
+#--------------------hyper parameters--------------------#
 
 
 # learning rate for fine-tuning CNN
