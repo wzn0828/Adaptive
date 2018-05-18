7b6929a016f7647236d2fbf3c435f57fc10156d0
wangzn <wangzn@gpu.vbig.org>
2018-05-18 10:48:32 +0800

    add a new parameter 'train_overfit_check'

--- code_src/config/cfg_wzn.py
+++ code_src/config/cfg_wzn.py
@@ -6,21 +6,21 @@ image_dir                   = '/home/wangzn/Datasets/ImageCaption/MSCOCO'
 captions_val_origin         = '/home/wangzn/Datasets/ImageCaption/MSCOCO/annotations/annotations_trainval2014/captions_val2014.json'
 captions_train_origin       = '/home/wangzn/Datasets/ImageCaption/MSCOCO/annotations/annotations_trainval2014/captions_train2014.json'
 splited_anno_path_prefix    = 'code_src/data/annotations/karpathy_split_'
-train_anno_path             = 'code_src/data/annotations/karpathy_split_train_overfit.json'  # path for train annotation json file
-val_anno_path               = 'code_src/data/annotations/karpathy_split_val_small.json'    # path for validation annotation json file
+train_anno_path             = 'code_src/data/annotations/karpathy_split_train_overfit_onebatch.json'  # path for train annotation json file
+val_anno_path               = 'code_src/data/annotations/karpathy_split_val.json'    # path for validation annotation json file
 test_anno_path              = 'code_src/data/annotations/karpathy_split_test.json'   # path for test annotation json file
 
 #--------------------attention_model--------------------#
 atten_model_name            = 'rnn_attention'    # ['adaptive','rnn_attention']
 
 #--------------------train--------------------#
-trainOrnot                  = False
+trainOrnot                  = True
 train_crop_size             = 224        # size for randomly cropping images
 train_log_step              = 10                                                 # step size for printing log info
 train_random_seed           = 123                                                # random seed for model reproduction
 train_pretrained            = False         # use train_pretrained_model or not
-train_pretrained_model      = 'Experiments/Train_rnn_attention_lr_5e-06_cnnlr_1e-06_cnn_start_layer_0_cnn_start_epoch_20___2018-04-27-11-00-54/trained_models/adaptive-51.pkl'      # [''|'path'] path of used model'] start from checkpoint or scratch, '' represents start from scratch
-train_num_epochs            = 100                                        # the maximum epochs
+train_pretrained_model      = 'Experiments/Train_adaptive_sgd_sgd_cnn_start_layer_5_cnn_start_epoch_20___2018-05-17-16-37-45/trained_models/adaptive-1.pkl'      # [''|'path'] path of used model'] start from checkpoint or scratch, '' represents start from scratch
+train_num_epochs            = 200                                        # the maximum epochs
 train_batch_size            = 25                                        # on cluster setup, 60 each x 4 for Huckle server
 train_clip                  = 0.1                                       # Gradient clipping for gradient exploding problem in LSTM
 train_lr_decay              = 40                                        # epoch at which to start lr decay
@@ -28,6 +28,7 @@ train_lr_decay_every        = 50                                        # decay 
 train_early_stop            = True
 train_figure_epoch          = 2
 train_evalOrnot             = False
+train_overfit_check         = True
 
 # optimization
 # CNN fine-tuning
@@ -35,26 +36,32 @@ opt_fine_tune_cnn_start_layer   = 5                                         # CN
 opt_fine_tune_cnn_start_epoch   = 20                                        # start fine-tuning CNN after
 
 # Optimizer parameter of rnn
-opt_rnn_optimization                = 'adam'  #['adam','sgd']
+opt_rnn_optimization                = 'adam'  #['adam','sgd','lbfgs']
 opt_rnn_adam_alpha                  = 0.9                                       # alpha in Adam
 opt_rnn_adam_beta                   = 0.999                                     # beta in Adam
-opt_rnn_adam_learning_rate          = 1e-3                                    # learning rate for the whole model
+opt_rnn_adam_learning_rate          = 5e-6                                    # learning rate for the whole model
 opt_rnn_adam_weight_decay           = 0
 
-opt_rnn_sgd_learning_rate           = 1e-6
-opt_rnn_sgd_momentum                = 0.99
+opt_rnn_sgd_learning_rate           = 5e-4
+opt_rnn_sgd_momentum                = 0.8
 opt_rnn_sgd_weight_decay            = 0
+
+
 
 # Optimizer parameter of cnn
 opt_cnn_optimization            = 'adam'  #['adam','sgd']
 opt_cnn_adam_alpha              = 0.9                                       # alpha in Adam
 opt_cnn_adam_beta               = 0.999
-opt_cnn_adam_learning_rate      = 5e-6
+opt_cnn_adam_learning_rate      = 5e-5
 opt_cnn_adam_weight_decay       = 0
 
-opt_cnn_sgd_learning_rate       = 5e-7
+opt_cnn_sgd_learning_rate       = 1e-5
 opt_cnn_sgd_momentum            = 0.99
 opt_cnn_sgd_weight_decay        = 0
+
+# Optimizer L-BFGS, for overfitting tiny dataset
+
+
 
 
 #--------------------test--------------------#
@@ -84,7 +91,7 @@ vacab_build_Ornot           = False
 vocab_threshold             = 5                                         # minimum word count threshold
 
 #--------------------KarpathySplit--------------------#
-KarpathySplitOrnot          = True
+KarpathySplitOrnot          = False
 num_val                     = 5000
 num_test                    = 5000
 num_train_overfit           = 20
